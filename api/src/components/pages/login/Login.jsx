6b9ac0a09a66f24d1dc2763d2c096112c7d0a748
Karmpal <kpsheoran33@gmail.com>
2023-01-06 20:24:03 +0530

    updated

--- api/src/components/pages/login/Login.jsx
+++ api/src/components/pages/login/Login.jsx
@@ -14,6 +14,11 @@ const Login = () => {
           <h1>Sign In</h1>
           <input type="email" placeholder="Email or phone number" />
           <input type="password" placeholder="Password" />
+          <button className="loginButton">Sign In</button>
+          <span>New to Netflix? <b>Sign up now.</b></span>
+          <small>
+            This page is protected by Google reCAPTCHS to ensure you're not a bot. <b>Learn more</b>.
+          </small>
         </form>
       </div>
     </div>
